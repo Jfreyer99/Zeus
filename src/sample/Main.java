6ecdccf82c177a9c85707f4061c45baca133b34a
Jfreyer99 <Jonas.Freyer@yahoo.com>
2020-06-04 10:24:59 +0200

    Initial Commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,23 +4,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
 
-    GridLayoutController con = new GridLayoutController();
-
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
         Parent root = loader.load();
+
+
+        MainStageController mc = loader.getController();
+        mc.drawOnCanvas();
+
 
         primaryStage.setTitle("Hello World");
 
-        primaryStage.setScene(new Scene(root, 300, 275));
-
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
---
+++ src/sample/MainStageController.java
@@ -0,0 +1,43 @@
+package sample;
+import javafx.fxml.FXML;
+
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+public class MainStageController {
+
+
+    @FXML private Canvas canvas;
+
+
+
+    @FXML
+    private void clickedOnCanvas(MouseEvent event)
+    {
+        System.out.println("X:"+event.getX());
+        System.out.println("Y:"+event.getY());
+    }
+
+    public void drawOnCanvas()
+    {
+        int w = 30;
+        int h = w;
+
+        GraphicsContext gc = this.canvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITESMOKE);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(1.0);
+
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                gc.strokeRect(j*w,i*h,w,h);
+            }
+        }
+    }
+
+}
